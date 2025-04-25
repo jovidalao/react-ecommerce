8d04be3515b548d6cb6b1a02847f414760ce5098
jovidalao <jovidalao@gmail.com>
2025-04-25 22:29:53 +1000

    07: trpc integration

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+// <-- hooks can only be used in client components
+import { useQuery } from '@tanstack/react-query';
+import { useTRPC } from '@/trpc/client';
+
 export default function Home() {
+  const trpc = useTRPC();
+  const categories = useQuery(trpc.categories.getMany.queryOptions())
+
   return (
     <div>
-      Home Page
+      <p>is loading: {`${categories.isLoading}`}</p>
+      {JSON.stringify(categories.data, null, 2)}
     </div>
   );
 }
